973aa3f6950f6914e614663903236c4f438809e9
z1ming <402832626@qq.com>
2019-08-30 04:18:17 +0000

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,7 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Category;
+use App\Models\User;
+
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category()
+    {
+        return $this->belongsto(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsto(User::class);
+    }
 }
