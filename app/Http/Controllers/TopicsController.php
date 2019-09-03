0a69fb9e653fe2e3ded10f724aa3279cb87ad92c
z1ming <402832626@qq.com>
2019-09-03 06:38:59 +0000

    Users can edit their own topic.

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -45,7 +45,8 @@ class TopicsController extends Controller
 	public function edit(Topic $topic)
 	{
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
+        $categories = Category::all();
+		return view('topics.create_and_edit', compact('topic','categories'));
 	}
 
 	public function update(TopicRequest $request, Topic $topic)
@@ -61,7 +62,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('success', '成功删除！');
+		return redirect()->route('topics.index')->with('message', '成功删除！');
 	}
 
     public function uploadImage(Request $request,ImageUploadHandler $uploader)
