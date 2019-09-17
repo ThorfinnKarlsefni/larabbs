23f80c8df26154ba8760f19d4a1f11a894e87cb2
z1ming <402832626@qq.com>
2019-09-17 08:50:07 +0000

    Setup default home page

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Link;
 use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category,Request $request,Topic $topic)
+    public function show(Category $category,Request $request,Topic $topic,User $user,Link $link)
     {
         $topics = $topic->withOrder($request->order)
                        ->where('category_id',$category->id)
                        ->paginate(30);
 
-        return view('topics.index',compact('topics','category'));
+        $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
+
+        return view('topics.index',compact('topics','category','active_users','links'));
     }
 }
