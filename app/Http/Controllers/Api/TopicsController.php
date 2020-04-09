2dc4049c2b7bb565b07b7e28dbb447fc5950c0f3
z1ming <402832626@qq.com>
2020-04-09 11:11:34 +0000

    topic update api

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -17,4 +17,12 @@ class TopicsController extends Controller
 
         return $this->response->item($topic,new TopicTransformer())->setStatusCode(201);
     }
+
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        // dd($request->all());exit;
+        $topic->update($request->all());
+        return $this->response->item($topic,new TopicTransformer());
+    }
 }
