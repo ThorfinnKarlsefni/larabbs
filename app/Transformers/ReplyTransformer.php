4f710a76883ad65160fe16f9c48f0d7634b25907
z1ming <402832626@qq.com>
2020-04-14 09:28:16 +0000

    replies index

--- app/Transformers/ReplyTransformer.php
+++ app/Transformers/ReplyTransformer.php
@@ -5,7 +5,9 @@ namespace App\Transformers;
 use App\Models\Reply;
 use League\Fractal\TransformerAbstract;
 
-class ReplyTransformer extends TransformerAbstract{
+class ReplyTransformer extends TransformerAbstract
+{
+    protected  $availableIncludes = ['user','topic'];
 
     public function Transform(Reply $reply)
     {
@@ -17,5 +19,15 @@ class ReplyTransformer extends TransformerAbstract{
             'created_at' => $reply->created_at->toDateTimeString(),
             'updated_at' => $reply->updated_at->toDateTimeString()
         ];
+    }
+
+    public function includeUser(Reply $reply)
+    {
+        return $this->item($reply->user,new UserTransformer());
+    }
+
+    public function includeTopic(Reply $reply)
+    {
+        return $this->item($reply->topic,new TopicTransformer());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -56,6 +56,12 @@ $api->version('v1', [
             ->name('api.users.topics.index');
         $api->get('topics/{topic}','TopicsController@show')
             ->name('api.topics.show');
+        // 话题回复列表
+        $api->get('topics/{topic}/replies','RepliesController@index')
+            ->name('api.topics.replies.index');
+        // 某个用户回复列表
+        $api->get('users/{user}/replies','RepliesController@userIndex')
+            ->name('api.users.replies.index');
 
         // 需要token 验证的接口
         $api->group(['middleware' => 'api.auth'],function($api){
@@ -80,6 +86,9 @@ $api->version('v1', [
             // 发布回复
             $api->post('topics/{topic}/replies','RepliesController@store')
                 ->name('api.topic.replies.store');
+            // 删除回复
+            $api->delete('topics/{topic}/replies/{reply}','RepliesController@destroy')
+                ->name('api.topic.replies.destroy');
         });
     });
 });
